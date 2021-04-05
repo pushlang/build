cb31b7b7401bb092b61eba01cf7fd6d82f9c17c4
pushlang <aapust@gmail.com>
2021-04-05 12:59:01 +0300

    initial

---
+++ ldflags/main.go
@@ -0,0 +1,14 @@
+package main
+
+import (
+	"buildTags/ldflags/build"
+	"fmt"
+)
+
+var Version = "development"
+
+func main() {
+	fmt.Println("Version:\t", Version)
+	fmt.Println("build.Time:\t", build.Time)
+	fmt.Println("build.User:\t", build.User)
+}
